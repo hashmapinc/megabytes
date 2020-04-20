491659e40d60578b4cea0d1f63239b21e129ef62
Randy Pitcher <randypitcherii@gmail.com>
2020-04-20 16:56:07 -0500

    rename of episode 3

--- episodes/2020-04-20 -- Episode 3 - Creating Users and Service Accounts in Snowflake/bertram_cooper_keygen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# generate private key
-openssl genrsa 2048 | openssl pkcs8 -topk8 -inform PEM -out snowflake_bertram_cooper
-
-# generate public key
-openssl rsa -in snowflake_bertram_cooper -pubout -out snowflake_bertram_cooper.pub
